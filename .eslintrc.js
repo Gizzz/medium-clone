f0b913ad63a0205c4f974e75a79c5c154d9a26d6
Gizzz <bulatkhusainov@yandex.ru>
2018-06-14 21:42:19 +0300

    eslint: turn off rules, restructure the file

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,10 @@ module.exports = {
     "node": true,
   },
   "rules": {
+    "arrow-parens": "off",
+
+    "react/forbid-prop-types": "off",
+
     "jsx-a11y/anchor-is-valid": "off",
     "jsx-a11y/alt-text": "off",
     "jsx-a11y/accessible-emoji": "off",
