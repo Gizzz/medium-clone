ea9aa6f168f27ba5bbac51376ca1f3d8792957ca
Gizzz <bulatkhusainov@yandex.ru>
2018-06-15 21:03:44 +0300

    webpack: add prod config, extract css at prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,59 +2,72 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/client/app/index.jsx',
-  output: {
-    filename: 'js/bundle.[hash].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true },
-          },
-          {
-            loader: 'sass-loader',
-            options: { sourceMap: true },
-          },
-        ],
-      },
-      {
-        test: /\.woff$/,
-        loader: 'file-loader',
-        options: { name: 'fonts/[name].[ext]' },
-      },
+module.exports = (env) => {
+  if (!env || !env.MODE) {
+    throw new Error('webpack --env.MODE option is not specified');
+  }
+
+  const isDevMode = env.MODE === 'dev';
+  const webpackMode = env.MODE === 'dev' ? 'development' : 'production';
+
+  return {
+    mode: webpackMode,
+    entry: './src/client/app/index.jsx',
+    output: {
+      filename: 'js/bundle.[hash].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true },
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true },
+            },
+          ],
+        },
+        {
+          test: /\.woff$/,
+          loader: 'file-loader',
+          options: { name: 'fonts/[name].[ext]' },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.json'],
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        template: './src/client/static/index.html',
+      }),
+      new CopyWebpackPlugin([
+        { from: './src/client/static/favicon.ico' },
+      ]),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash].css',
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      template: './src/client/static/index.html',
-    }),
-    new CopyWebpackPlugin([
-      { from: './src/client/static/favicon.ico' },
-    ]),
-  ],
-  devtool: 'source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    port: 4000,
-    historyApiFallback: true,
-  },
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      port: 4000,
+      historyApiFallback: true,
+    },
+  };
 };
