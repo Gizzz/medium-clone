f917feb2e29fc5efa778d01b9e786bf037e18049
Gizzz <bulatkhusainov@yandex.ru>
2018-06-19 15:25:57 +0300

    server: create seed-data file, move db files to data/

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const path = require('path');
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router(path.join(__dirname, 'data/db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
