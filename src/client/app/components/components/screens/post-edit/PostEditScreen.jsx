b7007a690e0b863392e15bf6ec74a3d488b3b883
Gizzz <bulatkhusainov@yandex.ru>
2018-06-20 14:49:18 +0300

    client: add styles for post-edit screen, alter markup a bit

--- src/client/app/components/components/screens/post-edit/PostEditScreen.jsx
+++ src/client/app/components/components/screens/post-edit/PostEditScreen.jsx
@@ -32,21 +32,23 @@ class PostEditScreen extends React.Component {
 
     return (
       <main className="post-edit">
-        <div className="user-info">
-          <a className="avatar avatar--middle avatar--circled" href="#">
-            <img src="https://cdn-images-1.medium.com/fit/c/120/120/1*9ZtET_L1852yXaDZJUo9CQ.png" />
-          </a>
-          <a href="#" className="author">Kent C. Dodds</a>
-        </div>
         <div className="post-meta">
-          <div>
-            <input className="title" placeholder="Title" />
+          <div className="user-info">
+            <a className="avatar avatar--middle avatar--circled" href="#">
+              <img src="https://cdn-images-1.medium.com/fit/c/120/120/1*9ZtET_L1852yXaDZJUo9CQ.png" />
+            </a>
+            <a href="#" className="author">Kent C. Dodds</a>
           </div>
-          <div>
-            <input className="subtitle" placeholder="Subtitle" />
-          </div>
-          <div>
-            <input className="big-img-url" placeholder="Big image url" />
+          <div className="input-fields">
+            <div>
+              <input className="title" placeholder="Title" />
+            </div>
+            <div>
+              <input className="subtitle" placeholder="Subtitle" />
+            </div>
+            <div>
+              <input className="big-img-url" placeholder="Big image url" />
+            </div>
           </div>
         </div>
         <div className="post-text js-editable" dangerouslySetInnerHTML={post.contentMarkup} />
