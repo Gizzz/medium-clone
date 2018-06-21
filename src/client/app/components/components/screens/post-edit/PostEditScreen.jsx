15f0aecaa4013322680101aceb2b4dc9a5c3d145
Gizzz <bulatkhusainov@yandex.ru>
2018-06-21 21:07:23 +0300

    client: medium-editor - add support for pre, code tags

--- src/client/app/components/components/screens/post-edit/PostEditScreen.jsx
+++ src/client/app/components/components/screens/post-edit/PostEditScreen.jsx
@@ -20,7 +20,21 @@ class PostEditScreen extends React.Component {
     this.editor = new MediumEditor('.js-editable', {
       targetBlank: true,
       toolbar: {
-        buttons: ['bold', 'italic', 'underline', 'anchor', 'h3', 'h4', 'quote'],
+        buttons: [
+          'bold', 'italic', 'underline', 'anchor', 'h3', 'h4', 'quote',
+          {
+            name: 'pre',
+            action: 'append-pre',
+            tagNames: ['pre'],
+            contentDefault: 'PRE',
+          },
+          {
+            name: 'strikethrough',
+            action: 'strikethrough',
+            tagNames: ['strike'],
+            contentDefault: 'CODE',
+          },
+        ],
       },
       placeholder: {
         text: 'Tell your story...',
