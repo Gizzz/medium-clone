d47aba5b047bf5c15ac965efd5322e177bde859f
Gizzz <bulatkhusainov@yandex.ru>
2018-06-22 20:51:15 +0300

    server: serve index.html if no other route matches

--- src/server/index.js
+++ src/server/index.js
@@ -3,10 +3,16 @@ const path = require('path');
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'data/db.json'));
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: path.resolve(__dirname, '../../dist'),
+});
 
 server.use(middlewares);
 server.use('/api', router);
+
+server.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../dist/index.html'));
+});
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
