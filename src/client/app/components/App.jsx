2adb7efd5086d2be2ed04498641ae9c266e257e1
Gizzz <bulatkhusainov@yandex.ru>
2018-06-26 12:59:58 +0300

    client: add code-splitting (route-based)

--- src/client/app/components/App.jsx
+++ src/client/app/components/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
 import Header from './components/shared/Header';
-import BlogScreen_Container from './components/screens/blog/BlogScreen_Container';
-import PostViewScreen_Container from './components/screens/post-view/PostViewScreen_Container';
-import PostEditScreen_Container from './components/screens/post-edit/PostEditScreen_Container';
+import Spinner from './components/shared/Spinner';
 
 import './assets/styles/index.scss';
+
+const BlogScreen_Container = Loadable({
+  loader: () => import('./components/screens/blog/BlogScreen_Container'),
+  loading: Spinner,
+});
+
+const PostViewScreen_Container = Loadable({
+  loader: () => import('./components/screens/post-view/PostViewScreen_Container'),
+  loading: Spinner,
+});
+
+const PostEditScreen_Container = Loadable({
+  loader: () => import('./components/screens/post-edit/PostEditScreen_Container'),
+  loading: Spinner,
+});
 
 const App = () => (
   <Router>
