460697e447571eee4cb17f715d5e13b6e6c8ab37
Gizzz <bulatkhusainov@yandex.ru>
2018-06-26 16:42:45 +0300

    client: perf - make blog header img responsive

--- src/client/app/components/components/screens/blog/components/BlogHeader.jsx
+++ src/client/app/components/components/screens/blog/components/BlogHeader.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const BlogHeader = ({ name, description }) => (
-  <div className="blog-header">
-    <div className="overlay" />
-    <div className="inner">
-      <div className="text">
-        <h1 className="name">{name}</h1>
-        <h2 className="description">{description}</h2>
+const BlogHeader = ({ name, description }) => {
+  const windowWidth = window.innerWidth;
+  const imgWidth =
+    windowWidth < 400 ? 400 :
+      windowWidth < 600 ? 600 :
+        windowWidth < 800 ? 800 :
+          windowWidth < 1200 ? 1200 :
+            windowWidth < 1600 ? 1600 :
+              2000;
+
+  return (
+    <div className="blog-header" style={{ backgroundImage: `url(https://cdn-images-1.medium.com/max/${imgWidth}/1*JVwi6UU_AJzhMkp1kMVc7A.jpeg)` }}>
+      <div className="overlay" />
+      <div className="inner">
+        <div className="text">
+          <h1 className="name">{name}</h1>
+          <h2 className="description">{description}</h2>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 BlogHeader.propTypes = {
   name: PropTypes.string.isRequired,
@@ -19,3 +30,5 @@ BlogHeader.propTypes = {
 };
 
 export default BlogHeader;
+
+/* eslint no-nested-ternary: off */
