583fbad34b2e93e977c0bdb8f6136c9260679202
Gizzz <bulatkhusainov@yandex.ru>
2018-07-11 23:49:09 +0300

    server: extract routes to file

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,10 @@
 const path = require('path');
-
 const express = require('express');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const lowdb = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-
-
-const adapter = new FileSync(path.resolve(__dirname, 'data/db.json'));
-const db = lowdb(adapter);
+//
+const api = require('./routes/api');
 
 const app = express();
 
@@ -26,48 +20,7 @@ app.use(express.static(
 
 // routes
 
-app.get('/api/posts/:id', (req, res) => {
-  const post = db
-    .get('posts')
-    .find({ id: Number(req.params.id) })
-    .value();
-
-  res.json(post);
-});
-
-app.get('/api/posts', (req, res) => {
-  const posts = db.get('posts').value();
-  res.json(posts);
-});
-
-app.patch('/api/posts/:id', (req, res) => {
-  const postChanges = req.body;
-
-  const post = db
-    .get('posts')
-    .find({ id: Number(req.params.id) })
-    .assign(postChanges)
-    .write();
-
-  res.json(post);
-});
-
-app.get('/api/blogs/:id', (req, res) => {
-  const blog = db
-    .get('blogs')
-    .find({ id: Number(req.params.id) })
-    .value();
-
-  res.json(blog);
-});
-
-app.get('/api/users/:id', (req, res) => {
-  const user = db
-    .get('users')
-    .find({ id: Number(req.params.id) })
-    .value();
-  res.json(user);
-});
+app.use('/api', api);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../dist/index.html'));
