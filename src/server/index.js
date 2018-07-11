08f627e700c15c7829ea32d7b2024e95544b93e8
Gizzz <bulatkhusainov@yandex.ru>
2018-07-11 23:22:52 +0300

    server: add routes for 'posts' entity

--- src/server/index.js
+++ src/server/index.js
@@ -26,9 +26,30 @@ app.use(express.static(
 
 // routes
 
+app.get('/api/posts/:id', (req, res) => {
+  const post = db
+    .get('posts')
+    .find({ id: Number(req.params.id) })
+    .value();
+
+  res.json(post);
+});
+
 app.get('/api/posts', (req, res) => {
   const posts = db.get('posts').value();
   res.json(posts);
+});
+
+app.patch('/api/posts/:id', (req, res) => {
+  const postChanges = req.body;
+
+  const post = db
+    .get('posts')
+    .find({ id: Number(req.params.id) })
+    .assign(postChanges)
+    .write();
+
+  res.json(post);
 });
 
 app.get('/api/blogs/:id', (req, res) => {
@@ -36,6 +57,7 @@ app.get('/api/blogs/:id', (req, res) => {
     .get('blogs')
     .find({ id: Number(req.params.id) })
     .value();
+
   res.json(blog);
 });
 
