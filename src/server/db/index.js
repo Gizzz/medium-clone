b228a4cdb3a331e925a78e1003e164cc8bec7348
Gizzz <bulatkhusainov@yandex.ru>
2018-07-18 22:02:50 +0300

    server: add id-related helpers for lowdb

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+const lodashId = require('lodash-id');
 
 const adapter = new FileSync(path.resolve(__dirname, './db-data.json'));
 const db = lowdb(adapter);
+db._.mixin(lodashId);
 
 module.exports = db;
