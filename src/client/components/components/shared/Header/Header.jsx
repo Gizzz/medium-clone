2b6521c01251ca884bbbb59befd840620af7d596
Gizzz <bulatkhusainov@yandex.ru>
2018-07-25 16:00:00 +0300

    client: reorganize import for Header component

---
+++ src/client/components/components/shared/Header/Header.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, withRouter } from 'react-router-dom';
+import axios from 'axios';
+
+import { GlobalContextConsumer } from '../../../ContextHelpers';
+import GuestLinks from './GuestLinks';
+import UserLinks from './UserLinks';
+
+const Header = ({ history }) => (
+  <GlobalContextConsumer>
+    {(context) => {
+      const { user } = context;
+
+      const handleLogout = (e) => {
+        e.preventDefault();
+
+        axios.post('/api/auth/logout')
+          .then(() => {
+            window.localStorage.removeItem('token');
+            axios.defaults.headers.common.Authorization = undefined;
+            context.setUser(null);
+
+            history.push('/');
+          }, window.alert);
+      };
+
+      return (
+        <header className="page">
+          <div className="inner">
+            <div className="link-set">
+              <Link className="logo" to="/" />
+            </div>
+            <div className="actions">
+              {
+                user
+                  ? <UserLinks user={user} onLogout={handleLogout} />
+                  : <GuestLinks />
+              }
+            </div>
+          </div>
+        </header>
+      );
+    }}
+  </GlobalContextConsumer>
+);
+
+Header.propTypes = {
+  history: PropTypes.object.isRequired,
+};
+
+const HeaderWithRouter = withRouter(Header);
+
+export default HeaderWithRouter;
+
+/* eslint max-len: off */
