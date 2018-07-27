b309429d4bda7f03e9233e6e3cb6cb170136161d
Gizzz <bulatkhusainov@yandex.ru>
2018-07-27 20:37:22 +0300

    server: add root-level error handler

--- src/server/index.js
+++ src/server/index.js
@@ -26,6 +26,11 @@ app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../dist/index.html'));
 });
 
+app.use((err, req, res, _next) => {
+  console.error(`${err.stack}\n`);
+  res.status(500).json({ error: 'Internal server error.' });
+});
+
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
