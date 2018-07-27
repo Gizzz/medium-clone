798e471d7f8dfb0259e858211377cf76a5815487
Gizzz <bulatkhusainov@yandex.ru>
2018-07-27 15:17:17 +0300

    client: use redirectUrl helper on login/register screens

--- src/client/app/components/screens/login/MarkupWrapper.jsx
+++ src/client/app/components/screens/login/MarkupWrapper.jsx
@@ -2,37 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const MarkupWrapper = ({ children }) => (
-  <main className="login">
-    <div className="container">
-      <div className="outer-box">
-        <div className="inner-box">
-          <h1>Welcome back.</h1>
-          <h2>Sign in to access your personalized homepage, follow authors and topics you love, and clap for stories that matter to you.</h2>
+import urlHelper from '../../../utils/urlHelper';
 
-          {children}
+const MarkupWrapper = ({ children }) => {
+  const encodedRedirectUrl = urlHelper.computeEncodedRedirectUrl();
 
-          <div className="account">
-            No account?
-            {' '}
-            <Link to="/register">Create one</Link>.
-          </div>
-          <div className="terms">
-            To make original Medium work, they(Medium team) log user data and share it with service providers. Click "Sign in" above to accept Medium’s
-            {' '}
-            <a href="https://medium.com/policy/medium-terms-of-service-9db0094a1e0f" target="_blank" rel="noopener noreferrer">
-              Terms of Service
-            </a>
-            {' & '}
-            <a href="https://medium.com/policy/medium-privacy-policy-f03bf92035c9" target="_blank" rel="noopener noreferrer">
-              Privacy Policy
-            </a>.
+  return (
+    <main className="login">
+      <div className="container">
+        <div className="outer-box">
+          <div className="inner-box">
+            <h1>Welcome back.</h1>
+            <h2>Sign in to access your personalized homepage, follow authors and topics you love, and clap for stories that matter to you.</h2>
+
+            {children}
+
+            <div className="account">
+              No account?
+              {' '}
+              <Link to={`/register?redirectUrl=${encodedRedirectUrl}`}>Create one</Link>.
+            </div>
+            <div className="terms">
+              To make original Medium work, they(Medium team) log user data and share it with service providers. Click "Sign in" above to accept Medium’s
+              {' '}
+              <a href="https://medium.com/policy/medium-terms-of-service-9db0094a1e0f" target="_blank" rel="noopener noreferrer">
+                Terms of Service
+              </a>
+              {' & '}
+              <a href="https://medium.com/policy/medium-privacy-policy-f03bf92035c9" target="_blank" rel="noopener noreferrer">
+                Privacy Policy
+              </a>.
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
 
 MarkupWrapper.propTypes = {
   children: PropTypes.node.isRequired,
