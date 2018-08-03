81f28e244ef31763ffee426f0abea8cbeb1bd3c4
Gizzz <bulatkhusainov@yandex.ru>
2018-08-03 22:11:18 +0300

    move webpack,babel, postcss to client

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,10 +32,10 @@ module.exports = (env) => {
   return {
     mode: webpackMode,
     devtool: 'source-map',
-    entry: './src/client/index.jsx',
+    entry: './src/index.jsx',
     output: {
       filename: 'js/bundle.[hash].js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, '../dist'),
       publicPath: '/',
     },
     module: {
@@ -81,10 +81,10 @@ module.exports = (env) => {
     plugins: [
       new CleanWebpackPlugin(['dist']),
       new HtmlWebpackPlugin({
-        template: './src/client/static/index.html',
+        template: './src/static/index.html',
       }),
       new CopyWebpackPlugin([
-        { from: './src/client/static/favicon.ico' },
+        { from: './src/static/favicon.ico' },
       ]),
       new MiniCssExtractPlugin({
         filename: 'css/[name].[contenthash].css',
