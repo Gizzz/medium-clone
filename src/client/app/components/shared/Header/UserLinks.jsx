e2e77bf1dd2a505767ff41f07f8b9db89ea20b9c
Gizzz <bulatkhusainov@yandex.ru>
2018-08-03 18:40:20 +0300

    client: fix typo

--- src/client/app/components/shared/Header/UserLinks.jsx
+++ src/client/app/components/shared/Header/UserLinks.jsx
@@ -5,7 +5,7 @@ const UserLinks = ({ user, onLogout }) => (
   <div className="user">
     <a className="sign-out" href="#" onClick={onLogout}>Sign out</a>
     <div className="avatar avatar--small">
-      <img src={user.avatarUrl} title={`Signed as ${user.username}`} />
+      <img src={user.avatarUrl} title={`Signed in as ${user.username}`} />
     </div>
   </div>
 );
