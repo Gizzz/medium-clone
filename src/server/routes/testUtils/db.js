81c1ee1b0eb0c36439f70b738be6de8d2c6ea88b
Gizzz <bulatkhusainov@yandex.ru>
2018-08-30 12:35:28 +0300

    server: add db utils for testing
    they allow to:
    - reset db to initial state
    - generate fake data

---
+++ src/server/routes/testUtils/db.js
@@ -0,0 +1,23 @@
+import faker from 'faker';
+
+import db from '../../db';
+import dbData from '../../db/db-data.json';
+
+const initDb = () => {
+  db.setState(dbData);
+};
+
+const generateRegistrationData = (overrides = {}) => {
+  const password = faker.internet.password(6);
+
+  return {
+    username: faker.internet.userName(),
+    password,
+    confirmPassword: password,
+    ...overrides,
+  };
+};
+
+export { initDb, generateRegistrationData };
+
+/* eslint import/no-extraneous-dependencies: off */
