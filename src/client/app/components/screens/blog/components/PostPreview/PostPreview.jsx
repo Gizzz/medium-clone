bdde37b5e17654d2904d321e656c0e8f71e8b74d
Gizzz <bulatkhusainov@yandex.ru>
2018-09-20 22:15:20 +0300

    client: rewrite PostPreview to use css modules

--- src/client/app/components/screens/blog/components/PostPreview/PostPreview.jsx
+++ src/client/app/components/screens/blog/components/PostPreview/PostPreview.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import styles from './PostPreview.module.scss';
+
 const PostPreview = ({ post, author }) => (
-  <article className={post.isLargePreview ? 'post post--full-width' : 'post'}>
-    <Link className="image" to={`/blogs/${post.blogId}/posts/${post.id}`}>
-      <div className="overlay-border" />
+  <article className={post.isLargePreview ? `${styles.post} ${styles['post--full-width']}` : styles.post}>
+    <Link className={styles.image} to={`/blogs/${post.blogId}/posts/${post.id}`}>
+      <div className={styles['overlay-border']} />
       <picture>
         <source
           srcSet={`https://cdn-images-1.medium.com/max/400/${post.imgDescriptor} 1x, https://cdn-images-1.medium.com/max/800/${post.imgDescriptor} 2x`}
@@ -22,20 +24,20 @@ const PostPreview = ({ post, author }) => (
         <img src={`https://cdn-images-1.medium.com/max/800/${post.imgDescriptor}`} />
       </picture>
     </Link>
-    <div className="text">
-      <Link className="text-link" to={`/blogs/${post.blogId}/posts/${post.id}`}>
+    <div className={styles.text}>
+      <Link className={styles['text-link']} to={`/blogs/${post.blogId}/posts/${post.id}`}>
         <h3>{post.title}</h3>
         <p>{post.subTitle}</p>
       </Link>
-      <div className="meta">
+      <div className={styles.meta}>
         <a className="avatar avatar--small avatar--circled" href="https://blog.kentcdodds.com/@kentcdodds" target="_blank" rel="noopener noreferrer">
           <img src={author.avatarUrl} />
         </a>
-        <div className="sub-meta">
-          <a className="author" href="https://blog.kentcdodds.com/@kentcdodds" target="_blank" rel="noopener noreferrer">
+        <div className={styles['sub-meta']}>
+          <a className={styles.author} href="https://blog.kentcdodds.com/@kentcdodds" target="_blank" rel="noopener noreferrer">
             {author.username}
           </a>
-          <span className="date">{post.date}</span>
+          <span className={styles.date}>{post.date}</span>
         </div>
       </div>
     </div>
